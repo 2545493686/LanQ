6037301d9a8349e72b8a30dc928a83f32f2cd7f2
杨东雄 <2545493686@qq.com>
2019-01-02 12:26:40 +0800

    ListExpand

---
+++ LanQ/ListExpand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanQ
+{
+    public static class ListExpand
+    {
+        public static bool HasOneSameItem(this IEnumerable thisCollection, IEnumerable beFoundList)
+        {
+            foreach (var thisItem in thisCollection)
+            {
+                foreach (var beFoundItem in beFoundList)
+                {
+                    if (beFoundItem == thisItem)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
